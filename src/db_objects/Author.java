0978623e5b1655ee253bb9dbf21b160d2178a24e
kezouke <eliwhatthe@gmail.com>
2023-07-01 14:27:08 +0300

    Author Info Update

--- src/db_objects/Author.java
+++ src/db_objects/Author.java
@@ -3,32 +3,32 @@ package db_objects;
 public class Author {
     public final Integer ID;
     private final String fullName;
-    private final Integer born;
-    private Integer died = 0;
+    private final String livingDate;
     private final String biography;
+    private final String border = "__________________________________\n";
 
-    public Author(Integer ID, String fullName, Integer born, Integer died, String biography) {
+
+    public Author(Integer ID, String fullName, String livingDate, String biography) {
         this.ID = ID;
         this.fullName = fullName;
-        this.born = born;
-        this.died = died;
+        this.livingDate = livingDate
         this.biography = biography;
     }
 
-    public Author(Integer ID, String fullName, Integer born, String biography) {
-        this.ID = ID;
-        this.fullName = fullName;
-        this.born = born;
-        this.biography = biography;
-    }
 
     public String shortInfo() {
-        return fullName;
+        return border +
+                "Author Short Info: " + "\n" +
+                border + "\t" +
+                "Name: " + fullName +
+                border;
     }
 
     public String detailedInfo() {
-        return shortInfo() + " \n"
-                + "Was born: " + born + "\n"
-                + "Biography: " + "\n" + biography;
+        return shortInfo() + "\n\t"
+                + "Dates: " + livingDate + "\n\t"
+                + "Biography: " + "\n\t" + biography +
+                border;
     }
+
 }
