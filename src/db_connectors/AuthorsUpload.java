330ba86b750cacffff31c054cecbfe2074ebabea
kezouke <eliwhatthe@gmail.com>
2023-07-04 15:06:52 +0300

    All objects from db now

--- src/db_connectors/AuthorsUpload.java
+++ src/db_connectors/AuthorsUpload.java
@@ -1,44 +1,27 @@
 package db_connectors;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 import db_objects.Author;
 
 public class AuthorsUpload {
     private final Map<String, Author> authorsMap;
 
-    public AuthorsUpload() {
+    public AuthorsUpload(Firestore db) {
         authorsMap = new HashMap<>();
-        lazyLoad();
+        lazyLoad(db);
     }
 
-    private void lazyLoad() {
+    private void lazyLoad(Firestore db) {
         try {
-            FileInputStream serviceAccount = new FileInputStream(
-                    System.getProperty("user.dir") + "\\cmd-art-gallery-firebase.json"
-            );
-            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(credentials)
-                    .build();
-            FirebaseApp.initializeApp(options);
-
-            Firestore db = FirestoreClient.getFirestore();
-
             CollectionReference authors = db.collection("author");
             ApiFuture<QuerySnapshot> query = authors.get();
             QuerySnapshot querySnapshot = query.get();
@@ -58,7 +41,7 @@ public class AuthorsUpload {
                         )
                 );
             }
-        } catch (IOException | ExecutionException | InterruptedException e) {
+        } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
--- src/db_connectors/PicturesUpload.java
+++ src/db_connectors/PicturesUpload.java
@@ -1,19 +1,12 @@
 package db_connectors;
 
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 import db_objects.Picture;
-
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -21,29 +14,18 @@ import java.util.concurrent.ExecutionException;
 public class PicturesUpload {
     private final Map<String, Picture> pictures;
 
-    public PicturesUpload() {
+    public PicturesUpload(Firestore db) {
         pictures = new HashMap<>();
-        lazyLoad();
+        lazyLoad(db);
     }
 
-    private void lazyLoad() {
+    private void lazyLoad(Firestore db) {
         try {
-            FileInputStream serviceAccount = new FileInputStream(
-                    System.getProperty("user.dir") + "\\cmd-art-gallery-firebase.json"
-            );
-            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(credentials)
-                    .build();
-            FirebaseApp.initializeApp(options);
-
-            Firestore db = FirestoreClient.getFirestore();
-
             CollectionReference collection = db.collection("pictures");
             ApiFuture<QuerySnapshot> query = collection.get();
             QuerySnapshot querySnapshot = query.get();
 
-            AuthorsUpload authors = new AuthorsUpload();
+            AuthorsUpload authors = new AuthorsUpload(db);
 
             for (DocumentSnapshot document : querySnapshot.getDocuments()) {
                 Map<String, Object> picture =
@@ -60,7 +42,7 @@ public class PicturesUpload {
                         )
                 );
             }
-        } catch (IOException | ExecutionException | InterruptedException e) {
+        } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
